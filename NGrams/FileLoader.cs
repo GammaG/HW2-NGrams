e17f1feeaa882b0dbd3a253f0e98d99072402f6b
GammaG <m_seidler@hotmail.de>
2014-12-16 14:20:09 +0100

    first commit

---
+++ NGrams/FileLoader.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGrams
+{
+
+    class FileLoader
+    {
+        
+        private int _bufferSize = 16384; 
+        private FileLoader fileLoader;
+        private String inputText = "";
+        private String stopWords = "";
+        private String textLocation = "";
+        private String stopWordsLocation = "";
+
+        private FileLoader()
+        {
+
+        }
+
+        public FileLoader getInstance()
+        {
+            if (fileLoader == null)
+                fileLoader = new FileLoader();
+            return fileLoader;
+        }
+
+    
+        private void setConfig(String language)
+        {
+            if (language.Equals(Constants.ENG))
+            {
+                textLocation = Constants.ENG_TEXT;
+                stopWordsLocation = Constants.ENG_STOPWORDS;
+            }
+        }
+
+        private void loadInformation()
+        {
+            if (textLocation.Equals("") | stopWordsLocation.Equals(""))
+                throw new InvalidDataException("You didn't set a config.");
+            try
+            {
+                loadText(textLocation);
+                loadStopWords(stopWordsLocation);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+
+        private void loadText(String location)
+        {
+            StringBuilder builder = new StringBuilder();
+            FileStream fStream = new FileStream(location, FileMode.Open, FileAccess.Read);
+            using (StreamReader streamReader = new StreamReader(fStream))
+            {
+                char[] fileContents = new char[_bufferSize];
+                int charsRead = streamReader.Read(fileContents, 0, _bufferSize);
+
+                // Can't do much with 0 bytes        
+                if (charsRead == 0)
+                    throw new Exception("File is 0 bytes");
+
+                while (charsRead > 0)
+                {
+                    builder.Append(fileContents);
+                    charsRead = streamReader.Read(fileContents, 0, _bufferSize);
+                }
+            }
+            inputText = builder.ToString();
+
+        }
+
+        private void loadStopWords(String location)
+        {
+            StringBuilder builder = new StringBuilder();
+            FileStream fStream = new FileStream(location, FileMode.Open, FileAccess.Read);
+            using (StreamReader streamReader = new StreamReader(fStream))
+            {
+                char[] fileContents = new char[_bufferSize];
+                int charsRead = streamReader.Read(fileContents, 0, _bufferSize);
+
+                // Can't do much with 0 bytes        
+                if (charsRead == 0)
+                    throw new Exception("File is 0 bytes");
+
+                while (charsRead > 0)
+                {
+                    builder.Append(fileContents);
+                    charsRead = streamReader.Read(fileContents, 0, _bufferSize);
+                }
+            }
+            stopWords = builder.ToString();
+
+        }
+    }
+
+}
---
+++ NGrams/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace NGrams
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnLoadF = new System.Windows.Forms.Button();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadF
+            // 
+            this.btnLoadF.Location = new System.Drawing.Point(12, 12);
+            this.btnLoadF.Name = "btnLoadF";
+            this.btnLoadF.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadF.TabIndex = 0;
+            this.btnLoadF.Text = "load Files";
+            this.btnLoadF.UseVisualStyleBackColor = true;
+            this.btnLoadF.Click += new System.EventHandler(this.btnLoadF_Click);
+            // 
+            // chart
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(307, 12);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(365, 340);
+            this.chart.TabIndex = 1;
+            this.chart.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 390);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.btnLoadF);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadF;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+    }
+}
+
